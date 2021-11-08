4f1ebeed78324e1fbfbc4b7606ef26949fe17dba
Kuntal <theencodedbong@gmail.com>
2021-11-08 18:33:35 +0000

    Initial uploads

---
+++ src/test/java/com/teb/practice/restfuljpaservices/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teb.practice.restfuljpaservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
